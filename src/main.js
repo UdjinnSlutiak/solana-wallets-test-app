7268c5d53da0539d90c314dbd8a7f9831a54e057
Slutiak Eugene <slutiak.jenia@gmail.com>
2022-02-08 11:56:58 +0200

    Done with test code

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import SolanaWallets from "solana-wallets-vue";
+import "solana-wallets-vue/styles.css";
+import { PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+const walletOptions = {
+    wallets: [
+        new PhantomWalletAdapter(),
+        new SlopeWalletAdapter(),
+        new SolflareWalletAdapter({ network: "devnet" }),
+    ],
+    autoConnect: true,
+}
+
+createApp(App)
+    .use(SolanaWallets, walletOptions)
+    .mount('#app');
