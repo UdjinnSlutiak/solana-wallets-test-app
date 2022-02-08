ae67908405553088167842d45fa172182c9ea8de
Slutiak Eugene <slutiak.jenia@gmail.com>
2022-02-08 14:44:33 +0200

    Tried it in another way

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
-import { WalletMultiButton } from 'solana-wallets-vue';
+import ChildComponent from './components/ChildComponent.vue';
+import { getPhantomWallet, getSolflareWallet } from "@solana/wallet-adapter-wallets";
+import { WalletProvider } from "@solana/wallet-adapter-vue";
+
+const wallets = [
+  getPhantomWallet(),
+  getSolflareWallet(),
+]
 </script>
 
 <template>
-  <wallet-multi-button></wallet-multi-button>
+  <wallet-provider :wallets="wallets" auto-connect>
+    <child-component></child-component>
+  </wallet-provider>
 </template>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { WalletMultiButton, WalletModalProvider } from '@solana/wallet-adapter-vue-ui';
+
+</script>
+
+<template>
+    <wallet-modal-provider>
+      <wallet-multi-button></wallet-multi-button>
+    </wallet-modal-provider>
+</template>
